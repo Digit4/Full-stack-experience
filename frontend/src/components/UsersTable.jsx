bc57b0ed588d3f9a866a6d8ec667e07efbdad9da
digit4 <dhaval.pantojee@gmail.com>
2025-09-12 20:26:21 +0530

    created users table to fetch from backend

---
+++ frontend/src/components/UsersTable.jsx
@@ -0,0 +1,37 @@
+import { Box } from "@mui/material";
+import axios from "axios";
+import { DataGrid } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
+
+const UsersTable = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/user/all");
+        if (response.status !== 200) {
+          console.error("Error fetching users", response);
+        }
+        console.log("digit4 response.data:", response.data);
+        setUsers(response.data);
+      } catch (err) {
+        console.error("Error fetching users", err);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    { field: "name", headerName: "Name", width: 320 },
+    { field: "is_admin", headerName: "Admin", type: "boolean", width: 90 },
+  ];
+  return (
+    <Box sx={{ width: "100%" }}>
+      <DataGrid columns={columns} rows={users} />
+    </Box>
+  );
+};
+
+export default UsersTable;
